2da74ebeae82b5ed917888ff24b46ee05c18190d
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-10-20 11:09:39 +0300

    бл ошибка была очевидна

--- webhook.php
+++ webhook.php
@@ -15,7 +15,7 @@ file_put_contents($logFile, $logData, FILE_APPEND);
 // Обработка данных вебхука
 if (isset($data['ref']) && $data['ref'] == 'refs/heads/main') {
     // Выполняем скрипт деплоя
-    exec('/path/to/deploy.sh');
+    exec('/var/www/html/deploy.sh');
     $logData = date('Y-m-d H:i:s') . " - Deployment script executed\n";
     file_put_contents($logFile, $logData, FILE_APPEND);
 } else {
