f160aec048b3ab027b90a2fd94bb52904d8508b3
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-10-20 14:39:39 +0300

    тест

--- downloads/downloads.js
+++ downloads/downloads.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         const formData = new FormData(uploadForm);
 
-        fetch('/var/www/downloads.php', {
+        fetch('/downloads/downloads.php', {
             method: 'POST',
             body: formData
         })
