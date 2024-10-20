499a8aa62c5dc681c5421c213448d2a08acb2308
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-10-20 14:41:17 +0300

    try

--- downloads/downloads.js
+++ downloads/downloads.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         const formData = new FormData(uploadForm);
 
-        fetch('/downloads/downloads.php', {
+        fetch('downloads.php', {
             method: 'POST',
             body: formData
         })
