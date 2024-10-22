7aa51fb98cf7f056ceb0cfa9a1f1a4bbcde909ad
MrGodgames <sashka.bog14@gmail.com>
2024-10-22 18:18:32 +0300

    a

--- Models/User.cs
+++ Models/User.cs
@@ -1,1 +1,10 @@
- 
+namespace Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+    }
+}
