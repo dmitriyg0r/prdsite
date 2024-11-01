208186f0c931295197abd965cfcfc43cf5ffaf08
MrGodgames <sashka.bog14@gmail.com>
2024-11-01 14:42:29 +0300

    страничка входа

---
+++ Profile/profile.js
@@ -0,0 +1,12 @@
+const togglePassword = () => {
+    const passwordInput = document.querySelector('.password-input');
+    const eyeIcon = document.querySelector('.eye-icon');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.add('show');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('show');
+    }
+}
