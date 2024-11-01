23bb58475186342dfc5bbaa70883db1fd5cb4383
MrGodgames <sashka.bog14@gmail.com>
2024-11-01 15:53:41 +0300

    вы

--- Backend/ScheduleApp.DataAccess/Models/User.cs
+++ Backend/ScheduleApp.DataAccess/Models/User.cs
@@ -1,9 +1,11 @@
+namespace ScheduleApp.DataAccess.Models;
+
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
-    public string Role { get; set; } // Admin или User
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime? LastLogin { get; set; }
-} 
+}
