42eb1991997160ee36c17f361927c4c0477b9acd
MrGodgames <sashka.bog14@gmail.com>
2024-11-05 14:42:44 +0300

    jhgvc

--- Backend/ScheduleApp.DataAccess/Program.cs
+++ Backend/ScheduleApp.DataAccess/Program.cs
@@ -72,9 +72,9 @@ class Program
         {
             if (context.Request.Method == "OPTIONS")
             {
-                context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-                context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
-                context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+                context.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+                context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
                 context.Response.StatusCode = 200;
                 return;
             }
