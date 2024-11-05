1353947ded628c6c4601e96b18a00122fc5b2b77
MrGodgames <sashka.bog14@gmail.com>
2024-11-05 10:17:10 +0300

    saf

--- Backend/ScheduleApp.DataAccess/obj/Debug/net8.0/ScheduleApp.DataAccess.AssemblyInfo.cs
+++ Backend/ScheduleApp.DataAccess/obj/Debug/net8.0/ScheduleApp.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleApp.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca276adfd56b96df2855d1fb0c93f2c310d5c6a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9655a1b51d8483ce78155e896cc8c5dd7275440")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleApp.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScheduleApp.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
