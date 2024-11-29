f3ff554f66f12fb18b5190be257693111f4253cc
MrGodgames <sashka.bog14@gmail.com>
2024-11-29 16:51:08 +0300

    das

--- Backend/ScheduleApp.DataAccess/Models/Requests/CreateUserRequest.cs
+++
@@ -1,6 +0,0 @@
-public class CreateUserRequest
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Role { get; set; }
-} 
--- Backend/ScheduleApp.DataAccess/Models/Requests/UpdateUserRequest.cs
+++
@@ -1,6 +0,0 @@
-public class UpdateUserRequest
-{
-    public string Username { get; set; }
-    public string? Password { get; set; }
-    public string Role { get; set; }
-} 
