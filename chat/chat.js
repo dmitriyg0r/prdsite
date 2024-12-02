132b272c404b3d0ed89892e13b93522c35e5f771
MrGodgames <sashka.bog14@gmail.com>
2024-12-02 18:59:13 +0300

    фыв

--- chat/chat.js
+++ chat/chat.js
@@ -42,7 +42,7 @@ let currentChatPartner = null;
 function createFriendElement(friend) {
     return `
         <div class="chat-partner" onclick="openChat('${friend.username}', '${friend.avatarUrl ? `/api/${friend.avatarUrl}` : '../assets/default-avatar.png'}')">
-            <img src="${friend.avatarUrl ? `${friend.avatarUrl}` : '../assets/default-avatar.png'}" alt="Avatar" class="friend-avatar">
+            <img src="${friend.avatarUrl ? `/api/${friend.avatarUrl}` : '../assets/default-avatar.png'}" alt="Avatar" class="friend-avatar">
             <div class="friend-info">
                 <div class="friend-name">${friend.username}</div>
                 <div class="last-message">${friend.lastMessage || 'Нет сообщений'}</div>
@@ -83,7 +83,7 @@ async function openChat(username, avatarUrl) {
         chatHeaderName.textContent = username;
     }
     if (chatHeaderAvatar) {
-        chatHeaderAvatar.src = `/api/${avatarUrl}`;
+        chatHeaderAvatar.src = avatarUrl;
     }
 
     try {
