08947cdaa3307fa652df675da82956eb29023673
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-12-02 16:19:15 +0300

    asdf

--- chat/chat.js
+++ chat/chat.js
@@ -21,7 +21,8 @@ function loadFriendsList() {
             const friendsListDiv = document.getElementById('friends-list');
             friendsListDiv.innerHTML = data.data.map(friend => `
                 <div class="chat-partner" onclick="openChat('${friend.username}')">
-                    <img src="${request.avatarUrl || '../assets/default-avatar.png'}" alt="Avatar" class="friend-avatar"> 
+                    <img src="${friend.avatarUrl ? `${API_BASE_URL}${friend.avatarUrl}` : '../assets/default-avatar.png'}" 
+                    alt="Avatar" class="friend-avatar">
                     <span>${friend.username}</span>
                 </div>
             `).join('');
