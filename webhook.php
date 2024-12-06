f2d1373133c8b24a03bdf573856edf1b875acc21
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-12-06 10:27:31 +0300

    adf

--- webhook.php
+++ webhook.php
@@ -8,14 +8,35 @@ function logMessage($message) {
     file_put_contents($logFile, $logData, FILE_APPEND);
 }
 
+logMessage("Request Method: " . $_SERVER['REQUEST_METHOD']);
+logMessage("Request Headers: " . json_encode(getallheaders()));
+
 logMessage("Webhook received");
 
-// Проверка секретного токена (если используется)
+// Проверка метода запроса
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    logMessage("Invalid request method: " . $_SERVER['REQUEST_METHOD']);
+    http_response_code(405);
+    exit('Method Not Allowed');
+}
+
+// Проверка наличия входных данных
+$payload = file_get_contents("php://input");
+if (empty($payload)) {
+    logMessage("Empty payload received");
+    http_response_code(400);
+    exit('Bad Request');
+}
+
+// Обновленная проверка секретного токена
 $secretToken = 'Gg3985502';
-if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== 'sha1=' . hash_hmac('sha1', file_get_contents("php://input"), $secretToken)) {
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
+$expectedSignature = 'sha1=' . hash_hmac('sha1', $payload, $secretToken);
+
+if ($signature !== $expectedSignature) {
     logMessage("Invalid secret token");
     http_response_code(403);
-    exit();
+    exit('Forbidden');
 }
 
 // Вызов скрипта deploy.php
