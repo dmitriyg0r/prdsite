10d8bfe2bc6a687ed1c5bf101df69b91caa64a72
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2024-12-19 11:09:22 +0300

    13

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
+    user: 'root',
     host: 'localhost',
     database: 'adminflow',
     password: 'sGLTccA_Na#9zC',
