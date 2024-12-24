ec24c4f66d11402d30967decbfe9e935055e65c4
MrGodgames <sashka.bog14@gmail.com>
2024-12-24 21:21:54 +0300

    фцв

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../utils/db');
 
 // Получение списка пользователей
 router.get('/users-list', async (req, res) => {
