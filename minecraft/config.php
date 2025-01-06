9308b6936fa16ed596dea7243a16de53beed5ae1
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-01-07 02:49:34 +0300

    16

--- minecraft/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'mysql-ru-r1.joinserver.xyz');
-define('DB_USER', 'u271672_Wu5jMv4H8S');
-define('DB_PASS', 'o.1.2u3DOF@DpOTK5S^70V3N');
-define('DB_NAME', 'minecraft_users');
-?> 
